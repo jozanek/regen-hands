d8bcc3c08f58db0ebf2f7762463d705eb81a33bd
Jozef Koval <jozef.koval@protonmail.ch>
2024-04-20 23:16:46 +0200

    Implement connection to Metamask wallet.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import { Web3 } from 'web3';
 
 function App() {
+  const [connectedAccount, setConnectedAccount] = useState('');
+  const [deployedAddress, setDeployedAddress] = useState('');
+  const [tokenAllowance, setTokenAllowance] = useState('0');
+  const [chainlinkPrice, setChainlinkPrice] = useState('0');
+  const [tokenBalance, setTokenBalance] = useState('0');
+  const [errMsg, setErrMsg] = useState('');
+  const [deposit, setDeposit] = useState('');
+
+  async function connectMetamask() {
+    //check metamask is installed
+    if (window.ethereum) {
+      // instantiate Web3 with the injected provider
+      const web3 = new Web3(window.ethereum);
+
+      //request user to connect accounts (Metamask will prompt)
+      await window.ethereum.request({ method: 'eth_requestAccounts' });
+
+      //get the connected accounts
+      const accounts = await web3.eth.getAccounts();
+
+      //show the first connected account in the react page
+      setConnectedAccount(accounts[0]);
+    } else {
+      alert('Please download metamask');
+    }
+  }
+
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {/* Button to trigger Metamask connection */}
+      <button onClick={() => connectMetamask()}>Connect wallet</button>
+
+      {/* Display the connected account */}
+      <h2>Connected to: {connectedAccount}</h2>
+
+      
+    </>
   );
 }
 
